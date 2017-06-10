affb5e6315c27cd4e492a773c182296903a54ecc
Vishwas S. Chouhan <batty.wayn3@gmail.com>
2017-06-10 08:02:17 +0530

    Use port specified in process.env

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express        = require('express');
 // const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const app            = express();
-const port = 8000;
+var port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 require('./app/routes')(app, {});
